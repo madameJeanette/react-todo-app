7236e7fb3030e0e0bc57802016bc8152665663fa
danie <danie@DESKTOP-M2H76EP>
2019-09-03 13:48:55 +0200

    addTodo component in Appjs w styling

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 
-export default class AddTodo extends Component {
+export class AddTodo extends Component {
     render() {
         return (
-            <form style={{ display: flex}}>
+            <form style={{ display: 'flex'}}>
              <input
                type="text" 
                name="title" 
-               style={{ flex: 10 }}
+               style={{ flex: '10', padding: '5px' }}
                placeholder="Add Todo ..."
             /> 
             <input
                type="submit" 
                value="Submit"
                className="btn"
-               style={{flex: 1}}
+               style={{flex: '1'}}
             /> 
          </form>
         )
     }
 }
 
-export default AddTodo
+export default AddTodo;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -45,8 +45,8 @@ const btnStyle = {
     padding: '5px 9px',
     borderRadius: '50%',
     cursor: 'pointer',
-    float: 'right',
+    float: 'right'
 
 }
 
-export default TodoItem
+export default TodoItem;
