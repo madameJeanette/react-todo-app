3a3d307868bf22db20e9f275d742e1c6c7420c5f
danie <danie@DESKTOP-M2H76EP>
2019-09-03 15:33:27 +0200

    link menu + styling

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,21 +1,26 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header style={hdrStyle}>
             <h1>Todo List</h1>
+            <Link style={linkStyle} to="/">Home</Link> |  <Link
+            style={linkStyle} to="/about">About</Link>
         </header>
     )
            
 }
-
 
 const hdrStyle = {
     background:'#1B1464',
     color:'#fff',
     padding: '10px',
     textAlign: 'center'
-    
-
 }
+
+const linkStyle = {
+    color:'#fff',
+    textDecoration: 'none'
+}
+
 export default Header;
