96c09d16a0c6ccf4e7ba445e9ee19d26d09ed608
danie <danie@DESKTOP-M2H76EP>
2019-09-03 15:27:11 +0200

    router + about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+    return (
+      <React.Fragment>
+        <h1>About</h1>
+        <p>This is the TodoList ap v.1.0.0.
+             It is part of a React crash course.</p>
+      </React.Fragment>
+      //react.fragment is a ghost element you don' t see but,
+      // need for jsx to undersand the markup.
+            
+      
+    )
+}
